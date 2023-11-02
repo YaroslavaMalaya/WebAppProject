e02ef36eca5da68f006e0ddea2123ff3ed2cc98b
Yaroslava Mala <ymala@kse.org.ua>
2023-11-02 13:28:14 +0200

    added privacy policy

--- scripts/server.js
+++ scripts/server.js
@@ -21,6 +21,10 @@ app.get('/profile', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'profile.html'));
 });
 
+app.get('/privacy', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'privacy.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
